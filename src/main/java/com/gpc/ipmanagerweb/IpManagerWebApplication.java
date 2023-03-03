6c42f0a5fb79748f1ab42462e18c181380a2d0fc
Ronald Andres Carrascal Carreazo <racarrascal@sprc.com.co>
2023-03-03 12:23:20 -0500

    migracion a spring boot

---
+++ src/main/java/com/gpc/ipmanagerweb/IpManagerWebApplication.java
@@ -0,0 +1,13 @@
+package com.gpc.ipmanagerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpManagerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpManagerWebApplication.class, args);
+	}
+
+}
