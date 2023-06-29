cd11be7a3c0acf2966780795c297da42b4d7a567
Ronald Andres Carrascal Carreazo <racarrascal@sprc.com.co>
2023-06-29 12:33:07 -0500

    mejoras

--- src/main/java/com/gpc/ipmanagerweb/controllers/IpMasterController.java
+++ src/main/java/com/gpc/ipmanagerweb/controllers/IpMasterController.java
@@ -3,6 +3,7 @@ package com.gpc.ipmanagerweb.controllers;
 
 import com.gpc.ipmanagerweb.dto.IpMasterRequest;
 import com.gpc.ipmanagerweb.service.client.IpManagerClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,28 +31,30 @@ public class IpMasterController {
     @PostMapping
     public ResponseEntity save(@RequestBody IpMasterRequest ipMasterRequest) {
         HttpServletRequest httpRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String jwtToken = httpRequest.getHeader("Authorization");
-        return ipManagerClient.save(jwtToken,ipMasterRequest);
+        return ipManagerClient.save(getHeaders(httpRequest),ipMasterRequest);
     }
 
     @PutMapping
     public ResponseEntity update(@RequestBody IpMasterRequest ipMasterRequest) {
         HttpServletRequest httpRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String jwtToken = httpRequest.getHeader("Authorization");
-        return ipManagerClient.update(jwtToken, ipMasterRequest);
+        return ipManagerClient.update(getHeaders(httpRequest), ipMasterRequest);
     }
 
     @GetMapping
     public ResponseEntity getAllIpsByUser(){
         HttpServletRequest httpRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String jwtToken = httpRequest.getHeader("Authorization");
-        return ipManagerClient.getAllIpsByUser(jwtToken);
+        return ipManagerClient.getAllIpsByUser(getHeaders(httpRequest));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteAllIpsByUser(@PathVariable(name = "id") Long id) {
         HttpServletRequest httpRequest = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-        String jwtToken = httpRequest.getHeader("Authorization");
-        return ipManagerClient.deleteAllIpsByUser(jwtToken,id);
+        return ipManagerClient.deleteAllIpsByUser(getHeaders(httpRequest),id);
+    }
+
+    private HttpHeaders getHeaders(final HttpServletRequest httpServletRequest) {
+        final HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", httpServletRequest.getHeader("Authorization"));
+        return headers;
     }
 }
--- src/main/java/com/gpc/ipmanagerweb/service/client/IpManagerClient.java
+++ src/main/java/com/gpc/ipmanagerweb/service/client/IpManagerClient.java
@@ -5,6 +5,7 @@ import com.gpc.ipmanagerweb.dto.IpMasterRequest;
 import feign.Headers;
 import feign.Param;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,24 +13,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 import java.util.concurrent.CompletableFuture;
 
 @FeignClient(value = "ipManagerService", url = "https://200.9.72.58:3706/ipMaster")
-@Headers("Authorization: {token}")
 public interface IpManagerClient {
 
     @PostMapping
-    ResponseEntity save(@Param("token") String token, @RequestBody IpMasterRequest ipMasterRequest);
+    ResponseEntity save(@RequestHeader HttpHeaders headers, @RequestBody IpMasterRequest ipMasterRequest);
 
     @PutMapping
-    ResponseEntity update(@Param("token") String token,@RequestBody IpMasterRequest ipMasterRequest);
+    ResponseEntity update(@RequestHeader HttpHeaders headers,@RequestBody IpMasterRequest ipMasterRequest);
 
 
     @GetMapping
-    ResponseEntity getAllIpsByUser(@Param("token") String token);
+    ResponseEntity getAllIpsByUser(@RequestHeader HttpHeaders headers);
 
     @DeleteMapping("/{id}")
-    ResponseEntity deleteAllIpsByUser(@Param("token") String token,@PathVariable(name = "id") Long id);
+    ResponseEntity deleteAllIpsByUser(@RequestHeader HttpHeaders headers,@PathVariable(name = "id") Long id);
 
 }
